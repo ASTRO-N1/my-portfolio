b7d01bf5d93faa6a80905e61519776222edafacc
Neeraj <neerajshimpi2005@gmail.com>
2025-03-30 17:55:10 +0530

    Added email functionality

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import send_email as se
 
 st.header("Contact Me")
+
+with st.form(key="email_form", clear_on_submit=True):
+    user_email = st.text_input("Enter Your Email")
+    user_message = st.text_area("Enter your message")
+    button = st.form_submit_button("Submit")
+
+if button:
+    se.sendmail(user_email_local = user_email, message_local = user_message)
---
+++ send_email.py
@@ -0,0 +1,21 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+host = "smtp.gmail.com"
+port = 465
+password = "avbvrrttofeimjpu"
+username = "neerajshimpi2912@gmail.com"
+context = ssl.create_default_context()
+receiver = "neerajshimpi2912@gmail.com"
+
+def sendmail(user_email_local, message_local):
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        msg = MIMEMultipart()
+        msg["From"] = username
+        msg["To"] = receiver
+        msg["Subject"] = "New Contact Me Form Submission"
+        email_body = f"User Email: {user_email_local}\n\nMessage:\n{message_local}"
+        msg.attach(MIMEText(email_body, "plain"))
+        server.sendmail(username, receiver,msg.as_string())
